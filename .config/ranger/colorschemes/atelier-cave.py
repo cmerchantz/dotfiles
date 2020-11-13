b0a8bdf059f4c4aa2e36bb72052885851ef4b133
Christopher Merchantz <42525677+cmerchantz@users.noreply.github.com>
2020-11-13 11:29:45 -0500

    Add ranger config

---
+++ .config/ranger/colorschemes/atelier-cave.py
@@ -0,0 +1,47 @@
+#### atelier-cave dark colorscheme
+# base00
+background                #19171C
+# base05
+foreground                #8B8792
+# base02
+selection_background      #585260
+# base05
+selection_foreground      #8B8792
+# base09
+url_color                 #AA573C
+# base05
+cursor                    #8B8792
+
+## black/grey - base01 and base03
+color0                    #26232A
+color8                    #655F6D
+## red - base08 and base0F
+color1                    #BE4678
+color9                    #BF40BF
+# green - base0B
+color2                    #2A9292
+color10                   #2A9292
+# yellow/orange/brown - base0A and base09
+color3                    #A06E3B
+color11                   #AA573C
+# blue - base0C
+color4                    #398BC6
+color12                   #398BC6
+# magenta - base0E
+color5                    #955AE7
+color13                   #955AE7
+# cyan - base0D
+color6                    #576DDB
+color14                   #576DDB
+# white/silver - base05 and base04
+color7                    #8B8792
+color15                   #7E7887
+
+# base08
+active_tab_foreground     #BE4678
+# base01
+active_tab_background     #26232A
+# base04
+inactive_tab_foreground   #7E7887
+# base01
+inactive_tab_background   #26232A
